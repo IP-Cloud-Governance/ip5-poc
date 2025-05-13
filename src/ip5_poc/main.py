d4847f72d4e0da92ea0240fba0ef513bc3f605c6
frithjofhoppe <frithjof.hoppe@gmail.com>
2025-05-13 16:17:39 +0200

    fix: :bug: Add cors origin to make it possible to use oscal viewer

--- src/ip5_poc/main.py
+++ src/ip5_poc/main.py
@@ -4,12 +4,20 @@ import oscal_pydantic.catalog as catalog
 import oscal_pydantic.core.common as common
 from ip5_poc.data import catalog as bv_catalog
 from typing import Dict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://viewer.oscal.io"],  # or ["*"] for all origins (not recommended for production)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 catalogs: Dict[str, Document] = dict()
 catalogs['si001'] = bv_catalog.get_si001_catalog()
-
 
 @app.get("/catalogs/{catalog_name}.json")
 def read_catalog(catalog_name: str):
