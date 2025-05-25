c21e8a0f18e7a921e1b1280bf3f01fba32f565d4
droidben <9570377+droidben@users.noreply.github.com>
2025-05-25 11:55:48 +0200

    aks cd unfinished

--- src/ip5_poc/main.py
+++ src/ip5_poc/main.py
@@ -6,6 +6,8 @@ from ip5_poc.data import catalog as bv_catalog
 from typing import Dict
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import HTTPException
+import json
+from pathlib import Path
 
 app = FastAPI(
     title="ip5-poc OSCAL",
@@ -31,3 +33,25 @@ def read_catalog(catalog_name: str):
         raise HTTPException(status_code=404, detail="Catalog not found")
     
 app.include_router(catalog_router)
+
+component_definition_router = APIRouter(prefix="/component-definitions", tags=["Component Definitions"])
+component_definitions: Dict[str, dict] = dict()
+
+# Load component definitions from JSON files in the 'raw' directory
+component_definitions_path = Path(__file__).parent / "data" / "raw"
+for file_path in component_definitions_path.glob("*.json"):
+    try:
+        with open(file_path, "r") as f:
+            component_definition = json.load(f)
+            component_definitions[file_path.stem] = component_definition
+    except Exception as e:
+        print(f"Error loading component definition from {file_path}: {e}")
+
+@component_definition_router.get("/{component_definition_name}.json", name="Get component definition")
+def read_component_definition(component_definition_name: str):
+    if component_definition_name in component_definitions:
+        return component_definitions[component_definition_name]
+    else:
+        raise HTTPException(status_code=404, detail="Component Definition not found")
+
+app.include_router(component_definition_router)
