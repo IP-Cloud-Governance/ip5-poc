1a42bad1a3ad266e88e3cdd6338ae6bd4b14802c
frithjofhoppe <frithjof.hoppe@gmail.com>
2025-07-11 12:31:44 +0200

    feat: :sparkles: Create policy definition based on project configuration

--- src/ip5_poc/api/triggers.py
+++ src/ip5_poc/api/triggers.py
@@ -1,17 +1,27 @@
+from collections import defaultdict
 from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 from ip5_poc.core.dependencies import get_api_key, get_az_credentials, get_db
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import PolicyClient
+from azure.mgmt.resource.policy.models import PolicySetDefinitionVersion, PolicyDefinitionReference, PolicySetDefinition
 from ip5_poc.models.model import CloudPlattform, CloudPlattformPath, OscalPropertyIdentifier
 from ip5_poc.services import project_service,oscal_service
 from motor.motor_asyncio import AsyncIOMotorDatabase
 import logging
+import re
+
+from ip5_poc.services.azure_service import get_rg_pattern
 
 trigger_router = APIRouter(prefix="/triggers", tags=["Manual triggering"], dependencies=[Depends(get_api_key)])
 
 logger = logging.getLogger(__name__)
 
+
+class PolicySetDefinitions(BaseModel):
+    searchPath: CloudPlattformPath
+    cloudPaths: list[str]
 
 @trigger_router.post(
     "/projects/{project_id}/deploy",
@@ -22,8 +32,12 @@ async def deploy_policies(
     credential: DefaultAzureCredential = Depends(get_az_credentials),
     db: AsyncIOMotorDatabase = Depends(get_db)
 ):
+    """
+    Collected policies of an ssp of a project and deploys them on the cloud
+    """
     project = await project_service.get_project(project_id=project_id, db=db)
-    policy_set_definitions: list[tuple[CloudPlattformPath, list[str]]] = []
+    # policy_set_definitions: list[tuple[CloudPlattformPath, list[str]]] = []
+    policy_set_definitions: list[PolicySetDefinitions] = []
     ssp_raw = await oscal_service.get_ssp_by_project(project_id=project_id, db=db)
     ssp = ssp_raw.root.system_security_plan
     
@@ -53,12 +67,84 @@ async def deploy_policies(
             # TODO this check is here because there might by requirements where not policy is set -> t6.1
             if implemented_requirement.props:
                 azure_policies = list(filter(lambda x: x.name.root == OscalPropertyIdentifier.AZURE_POLICY.value, implemented_requirement.props))
-                policy_set_definitions.append((search_item, [p.value for p in azure_policies]))
+                policy_set_definitions.append(
+                    PolicySetDefinitions(
+                        searchPath=search_item,
+                        cloudPaths=[p.value.root for p in azure_policies]
+                    )
+                )
+                # policy_set_definitions.append((search_item, [p.value for p in azure_policies]))
             
 
     # Merge azure policies
-    azure_policies_subset = list(filter(lambda x: x[0].plattform == CloudPlattform.AZURE, policy_set_definitions))
+    azure_policies_subset = list(filter(lambda x: x.searchPath.plattform == CloudPlattform.AZURE, policy_set_definitions))
+
+
+    # Create azure policy initiative
+
+    for azure_policy_set in azure_policies_subset:
+        match = re.fullmatch(get_rg_pattern(), azure_policy_set.searchPath.path)
+        policy_client_subscription_id = match.group("subscription_id")
+        policy_client = PolicyClient(
+            credential=credential,
+            subscription_id=policy_client_subscription_id
+        )
+
+        policy_definitions: list[PolicyDefinitionReference] = [
+            PolicyDefinitionReference(policy_definition_id=cloud_path) for cloud_path in azure_policy_set.cloudPaths
+        ]
+
+        initiative_definition: PolicySetDefinition = PolicySetDefinition(
+            display_name=f"ip5sgcgov project {str(project.name)} for search {str(azure_policy_set.searchPath.id)}",
+            description=f"ip5sgcgov project {str(project.name)} for search {str(azure_policy_set.searchPath.id)}",
+            policy_definitions=policy_definitions,
+            metadata={
+                "category": "ip5sgcgov-project",
+                "ip5sgcgov-project-name":project.name,
+                "ip5sgcgov-project-id": str(project.id),
+                "ip5sgcgov-ssp-id": str(ssp.uuid.root),
+                "ip5sgcgov-search-path": azure_policy_set.searchPath.path,
+                "ip5sgcgov-search-path-id": azure_policy_set.searchPath.id,
+            }
+        )
+        policy_set_definition_name = f"ip5sgcgov-{str(azure_policy_set.searchPath.id)}"
+        logger.info(f"Try to create policy {policy_set_definition_name} for project {project.name} with id {str(project.id)}")
+        logger.info(_merge_policy_sets(azure_policies_subset))
+        result = policy_client.policy_set_definitions.create_or_update(
+            policy_set_definition_name=policy_set_definition_name,
+            parameters=initiative_definition
+        )
+        if result:
+            logger.info(f"Created policy with id {result.id}")
+            logger.info(result)
+
 
     # TODO CONTINUE HERE with merging policies of azure_policies_subset together to get one search path and all policies
     # then create a initiaite and deploy it for the scope set in the serach path
-    return list(filter(lambda x: x[0].plattform == CloudPlattform.AZURE, policy_set_definitions))
+    return _merge_policy_sets(azure_policies_subset)
+
+
+def _merge_policy_sets(policy_sets: list[PolicySetDefinitions]) -> list[PolicySetDefinitions]:
+    """
+    Merge policy paths by same search query from which they originated
+    """
+    grouped = defaultdict(list)
+
+    for policy in policy_sets:
+        key = policy.searchPath.id
+        grouped[key].append(policy)
+
+    merged = []
+    for key, items in grouped.items():
+        # Merge cloudPaths
+        all_cloud_paths: list[str] = []
+        for item in items:
+            all_cloud_paths.extend(item.cloudPaths)
+
+        # Reuse one of the identical searchPaths
+        merged.append(PolicySetDefinitions(
+            searchPath=items[0].searchPath,
+            cloudPaths=all_cloud_paths
+        ))
+
+    return merged
